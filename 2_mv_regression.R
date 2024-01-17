9ed5bbd4c70829955e830dfb42ea7068c1b730d0
gwenknight <gwen.knight@lshtm.ac.uk>
2024-01-17 09:09:33 +0000

    tidy up mv_regressions to use all the right bacteria / antibiotics and to run independently

--- 2_mv_regression.R
+++ 2_mv_regression.R
@@ -1,20 +1,24 @@
 ##### MICAG regression analysis for specified drugs
 # Specify drug / bug combination to run regression analysis
-library(data.table); library(MASS)
+library(data.table); library(MASS); library(tidyverse)
 
 # read in data (can use alternative)
 full_data_orig <- as.data.table(read.csv("data/full_data.csv"))
 
+# Create folder for outputs
 if(!file.exists("mv_output")){dir.create("mv_output")} 
+
+# Load function
+source("1_functions.R")
 
 ######*********************** SPECIFY WHICH BUG + DRUG ************************#################
 # specify which bacteria and antibiotic of interest
 # NOTE - must match spelling in the data table
 mv_analysis(full_data_orig, "levofloxacin", "Staphylococcus aureus")
 
-######### OR cycle through all
-top_bacteria <- t(read_csv("data/top_bacteria.csv")[,-1])
-store <- c()
+######*********************** OR CYCLE THROUGH ALL ************************#################
+top_bacteria <- unique(full_data_orig$organism_clean)
+store <- c() # where will store outputs
 
 ### Run through all 
 for(i in top_bacteria){
